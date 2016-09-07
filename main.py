2a9a9fe03c3ceaf5815ef920a08dd1657a956419
Shevah92 <shevah92@gmail.com>
2016-09-07 11:25:53 +0200

    add story delete function

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route("/list", methods=["GET"])
 def render_index():
     stories = UserStories.select()
-    return render_template("list.html", stories = stories)
+    return render_template("list.html", stories = stories, address= config.address,
+                           delete = config.address+"story/delete/")
 
 
 @app.route("/story", methods=["GET"])
@@ -27,6 +28,12 @@ def get_data():
                        estimation=request.form["estimation"],
                        status=request.form["status"])
 
+    return redirect(config.address)
+
+@app.route("/story/delete/<story_id>", methods=["GET"])
+def delete_story(story_id):
+    data = UserStories.get(UserStories.id == story_id)
+    data.delete_instance()
     return redirect(config.address)
 
 if __name__ == '__main__':
