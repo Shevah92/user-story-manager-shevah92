76bb85186777ab1b329daca9bdcb6da7bc44c909
Shevah92 <shevah92@gmail.com>
2016-09-07 10:45:00 +0200

    add userstory finally works

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from models import *
 from flask import *
+import config
 
 
 app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+@app.route("/list", methods=["GET"])
+def render_index():
+    stories = UserStories.select()
+    return render_template("list.html", stories = stories)
 
 
 @app.route("/story", methods=["GET"])
@@ -12,13 +20,15 @@ def render_story():
 
 @app.route("/story", methods=["POST"])
 def get_data():
-    fos = UserStories.create(story_title = request.form["story_title"],
-                       user_story = request.form["user_story"],
-                       acceptance = request.form["acceptance"],
-                       business_value = request.form["business_value"],
-                       estimation = request.form["estimation"],
-                       status = request.form["status"])
-    return "Hello World"
+    UserStories.create(story_title=request.form["story_title"],
+                       user_story=request.form["user_story"],
+                       acceptance=request.form["acceptance"],
+                       business_value=request.form["business_value"],
+                       estimation=request.form["estimation"],
+                       status=request.form["status"])
+
+    return redirect(config.address)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
